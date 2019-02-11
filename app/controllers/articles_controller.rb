d6182b7722c39c813bc19c050b9e7fc29271f2e2
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-11 15:41:55 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-    get '/articles' do
+    get '/articles' do  #<!-- Loads all articles into an instance variable, render to corresponding invdex view.  -->
             @articles = Article.all
             erb :"articles/index"
     end
